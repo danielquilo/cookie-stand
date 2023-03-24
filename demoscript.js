064679a0706ead252eebbdc92f819d0ed605d687
danielquilo <danielquilo1012@yahoo.com>
2023-03-24 14:22:52 +0000

    learningjavascripts

---
+++ demoscript.js
@@ -0,0 +1,34 @@
+let user = prompt("Hi my name is Daniel, what is your name");
+
+console.log("Hi" + user);
+
+let time = prompt("Hi " + user + ", what hour is it? (0 - 23)");
+
+let message;
+
+if ( time < 12 ) {
+    message = "Good Morning " + user;
+} else if (time <=18)  {
+    message = " Good afternoon " + user;
+} else if ( time <  24) {
+message = "Good night " + user;
+} else {
+    message = " That hour does not exist " + user;
+} 
+
+
+console.log(message);
+
+let arsenalFan = confirm("I am an arsenal fan, do tou support arsenal"  +  user + "?")
+
+if(arsenalFan == true) {
+    console.log("You're great");
+} else {
+    console.log("you suck")
+}
+
+/*
+function daniel(){
+console.log("hi I am daniel");
+}
+*/http://127.0.0.1:5555/index.html
---
+++ scripts.js
@@ -0,0 +1,63 @@
+/* const heading = document.getElementById("heading");
+
+console.log(heading);
+
+// varaible assigned value is a sting
+const myName = "Daniel";
+
+// varaible assigned value is a number
+let myAge = 28;
+
+// varaible assigned value is a boolean
+let isAlive = true;
+
+// varaible assigned value is a array
+const myPets = ["dog", "cat", "goldfish"]
+
+// varaible assigned value is a object
+const cup = {
+color: "blue",
+design: "mosiac",
+madeOfChina: true,
+weigthInGrams: 48,
+}
+
+// varaible assigned value is a object
+const locations = {
+heading: "",
+location: "",
+highlyRated: true,
+price: 48,
+};
+
+let x = 5 - 5;
+let y = 6 - 3;
+let a = 2 * 2;
+let b = 5 / 5;
+
+let myHouseNum = "5" + 5;
+console.log(typeof myHouseNum); */
+
+let myName = "Daniel"
+console.log(myName)
+
+let myAge = 28;
+
+console.log(myAge)
+
+const nameAge = "Hello my name Daniel " + myName + 
+" and I am" + myAge + " 28 years old";
+console.log(nameAge);
+
+let x = 5;
+const y = "5";
+
+let number = x + y 
+console.log(typeof number);
+
+//const demo = document.getElementById("demo")
+//console.log(demo)
+//demo.innerHTML = message;
+
+document.getElementById("demo").innerHTML = message;
+
